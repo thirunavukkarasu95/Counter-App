be3324a980d2daddcf46e19766cc9dab5abda201
thirunavukkarasu95 <thirunavukkarasu.95@gmail.com>
2019-03-20 23:04:03 -0400

    Added Reset Button and handled delete

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,19 +1,23 @@
 import React, { Component } from "react";
 class Counter extends Component {
   state = {
-    value: this.props.value,
-    tags: ["tag1", "tag2", "tag3"]
+    value: this.props.counter.value
   };
   render() {
     return (
       <div>
-        <h4>Counter #{this.props.id}</h4>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
           onClick={this.handleIncrement}
           className="btn btn-secondary btn-sm"
         >
           Increment
+        </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
         </button>
       </div>
     );
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -10,11 +10,35 @@ class Counters extends Component {
       { id: 4, value: 0 }
     ]
   };
+
+  handleDelete = counterId => {
+    const counters = this.state.counters.filter(c => c.id !== counterId);
+    this.setState({ counters });
+  };
+
+  handleReset = () => {
+    const counters = this.state.counters.map(c => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
+
   render() {
     return (
       <div>
+        <button
+          onClick={this.handleReset}
+          className="btn btn-primary btn-sm m-2"
+        >
+          Reset
+        </button>
         {this.state.counters.map(counter => (
-          <Counter key={counter.id} value={counter.value} id={counter.id} />
+          <Counter
+            key={counter.id}
+            onDelete={this.handleDelete}
+            counter={counter}
+          />
         ))}
       </div>
     );
