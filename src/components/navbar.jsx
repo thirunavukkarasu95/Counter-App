2fe10c4184221dbcf6ffd8472305f5b1022b54af
thirunavukkarasu95 <thirunavukkarasu.95@gmail.com>
2019-03-31 23:50:09 -0400

    Lifted state to App Component

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,15 +1,16 @@
-import React, { Component } from "react";
+import React from "react";
 
-class NavBar extends Component {
-  render() {
-    return (
-      <nav className="navbar navbar-light bg-light">
-        <a className="navbar-brand" href="#">
-          Navbar
-        </a>
-      </nav>
-    );
-  }
-}
+const NavBar = ({ totalCounters }) => {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <a className="navbar-brand" href="#">
+        Navbar{" "}
+        <span className="badge badge-pill badge-secondary">
+          {totalCounters}
+        </span>
+      </a>
+    </nav>
+  );
+};
 
 export default NavBar;
